b551ead5998faacf024a16c817c831d69e71e1a7
gahramanova <narminhikmat@gmail.com>
2025-02-27 15:55:56 +0100

    About page v0.2.4

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import animals from "../assets/img/animals.png"
 import { IoIosArrowForward } from "react-icons/io";
 import aboutImgOne from "../assets/img/aboutImgOne.jpg"
 import aboutImgTwo from "../assets/img/aboutImgTwo.jpg"
 import { FaCircleCheck } from "react-icons/fa6";
 import { useEffect } from "react";
+import abouthelpimg from "../assets/img/aboutHelp.png"
+import abouthelpimgTwo from "../assets/img/abouthelpOne.png"
+import abouthelpimgThree from "../assets/img/abouthelpTwo.png"
 import Aos from "aos";
 import "aos/dist/aos.css";
+import CountUp from 'react-countup';
+import aboutImg from "../assets/img/AboutBg.svg"
+import cardIcon from "../assets/img/cardIcon.svg"
+import teamOne from "../assets/img/teamOne.jpg"
+import teamTwo from "../assets/img/teamTwo.jpg"
+import teamThree from "../assets/img/teamThree.jpg"
+import { FaArrowRightLong } from "react-icons/fa6";
+
 
 
 const About = () => {
@@ -71,48 +82,187 @@ const About = () => {
       </section>
 
       <section className="about-section-three">
-        <div className="container">
-          <div className="px-4 py-5 my-5 text-center">
-            <p className="m-0">Why we are the best</p>
-            <h1 className=" fw-bold text-body-emphasis">See How Petpal can Help</h1>
+        <div className="container py-5">
+          <div className="px-4 text-center">
+            <div className='d-flex align-items-center justify-content-center'> <p className='fw-bold m-0' style={{ color: "#894B8D" }}>WHY WE ARE THE BEST</p><span><i style={{ color: "#C2A8C2" }} className="fa-solid fa-paw mx-2"></i></span></div>
+            <h1 className="fw-bold">See How Petpal can Help</h1>
             <div className="col-lg-6 mx-auto">
-              <p className="lead mb-4">Duis aute irure dolor in reprehenderit in voluptate velit esse
+              <p className=" mb-4" style={{ color: "#4C5A7A" }}>Duis aute irure dolor in reprehenderit in voluptate velit esse
                 amily and deserves ets are the best care.</p>
             </div>
           </div>
           <div className="row">
             <div className="col-12 col-sm-6 col-md-4 col-lg-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
+              <div className="card d-flex justify-content-center align-items-center flex-column py-4">
+                <img src={abouthelpimgThree} className="card-img" style={{ width: "150px", height: "150px" }} />
                 <div className="card-body d-flex justify-content-center align-items-center flex-column">
                   <h5 className="card-title">Health Guarantee</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  
+                  <p className="card-text">Duis aute irure dolor in reprehenderit voluptate velit essed eservesets are their health best care</p>
+
                 </div>
               </div>
             </div>
             <div className="col-12 col-sm-6 col-md-4 col-lg-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
+              <div className="card d-flex justify-content-center align-items-center flex-column py-4">
+                <img src={abouthelpimgTwo} className="card-img" style={{ width: "150px", height: "150px" }} />
                 <div className="card-body d-flex justify-content-center align-items-center flex-column">
                   <h5 className="card-title">Ethical breeding</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  
+                  <p className="card-text">Duis aute irure dolor in reprehenderit voluptate velit essed eservesets are their health best care</p>
+
                 </div>
               </div>
             </div>
             <div className="col-12 col-sm-6 col-md-4 col-lg-4">
-              <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
+              <div className="card d-flex justify-content-center align-items-center flex-column py-4">
+                <img src={abouthelpimg} className="card-img" style={{ width: "150px", height: "150px" }} />
                 <div className="card-body d-flex justify-content-center align-items-center flex-column">
                   <h5 className="card-title">Transparent Policy</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  
+                  <p className="card-text">Duis aute irure dolor in reprehenderit voluptate velit essed eservesets are their health best care</p>
+
                 </div>
               </div>
             </div>
           </div>
+        </div>
+      </section>
+      <section className="about-section-four my-5 py-5 px-5">
+        <div className="container px-5 py-5">
+          <div className="row">
+            <div className="col-12 col-sm-6 col-md-3">
+              <div className="content">
+                <div className="counter-image"><img src={aboutImg} /></div>
+                <div className="d-flex justify-content-center align-items-center flex-column counter">
+                  <CountUp start={0} end={5985}>
+                    {({ countUpRef }) => (
+                      <div>
+                        <span ref={countUpRef} style={{ fontSize: "40px", color: "white", fontWeight: "600" }} />
+                      </div>
+                    )}
+                  </CountUp>
 
+                  <h5 className="text-light">Happy Family</h5>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-6 col-md-3">
+              <div className="content">
+                <div className="counter-image"><img src={aboutImg} /></div>
+                <div className="d-flex justify-content-center align-items-center flex-column counter">
+                  <CountUp start={0} end={1322}>
+                    {({ countUpRef }) => (
+                      <div>
+                        <span ref={countUpRef} style={{ fontSize: "40px", color: "white", fontWeight: "600" }} />
+                      </div>
+                    )}
+                  </CountUp>
+
+                  <h5 className="text-light">Listed States</h5>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-6 col-md-3">
+              <div className="content">
+                <div className="counter-image"><img src={aboutImg} /></div>
+                <div className="d-flex justify-content-center align-items-center flex-column counter">
+                  <CountUp start={0} end={3102}>
+                    {({ countUpRef }) => (
+                      <div>
+                        <span ref={countUpRef} style={{ fontSize: "40px", color: "white", fontWeight: "600" }} />
+                      </div>
+                    )}
+                  </CountUp>
+
+                  <h5 className="text-light">Core Breeding</h5>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-6 col-md-3">
+              <div className="content">
+                <div className="counter-image"><img src={aboutImg} /></div>
+                <div className="d-flex justify-content-center align-items-center flex-column counter">
+                  <CountUp start={0} end={1125}>
+                    {({ countUpRef }) => (
+                      <div>
+
+                        <span ref={countUpRef} style={{ fontSize: "40px", color: "white", fontWeight: "600" }} />
+                      </div>
+                    )}
+                  </CountUp>
+
+                  <h5 className="text-light">Annual Awards</h5>
+                </div></div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="about-section-five">
+        <div className="container mt-5">
+          <div className='d-flex align-items-center justify-content-center'> <p className='fw-bold m-0' style={{ color: "#894B8D" }}>WE CHANGE YOUR LIFE & WORLD
+          </p><span><i style={{ color: "#C2A8C2" }} className="fa-solid fa-paw mx-2"></i></span></div>
+          <h1 className="text-center fw-bold" style={{ color: "#002168" }}>We’re Dedicated To Excellent<br/>
+          Service And Care</h1>
+
+          <div className="team mt-5">
+            <div className="row g-4">
+              <div className="col-12 col-sm-6 col-md-3 col-lg-3">
+                <div className="d-flex justify-content-end align-items-end">
+                  <img src={cardIcon} />
+                </div>
+                <div className="card">
+                  <img src={teamOne} className="card-img-top" />
+                  <div className="card-body">
+                    <h5 className="card-title text-center">Lizay Ariania</h5>
+                    <p className="card-text text-center">Veterinary Technician</p>
+
+                  </div>
+                </div>
+              </div>
+              <div className="col-12 col-sm-6 col-md-3 col-lg-3">
+                <div className="d-flex justify-content-end align-items-end">
+                  <img src={cardIcon} />
+                </div>
+                <div className="card">
+                  <img src={teamTwo} className="card-img-top" />
+                  <div className="card-body">
+                    <h5 className="card-title text-center">Kenroly Gajon</h5>
+                    <p className="card-text text-center">Food Technician</p>
+
+                  </div>
+                </div>
+              </div>
+              <div className="col-12 col-sm-6 col-md-3 col-lg-3">
+                <div className="d-flex justify-content-end align-items-end">
+                  <img src={cardIcon} />
+                </div>
+                <div className="card">
+                  <img src={teamThree} className="card-img-top" />
+                  <div className="card-body">
+                    <h5 className="card-title text-center">Michael Brian</h5>
+                    <p className="card-text text-center">Medicine Specialist</p>
+
+                  </div>
+                </div>
+              </div>
+              <div className="col-12 col-sm-6 col-md-3 col-lg-3">
+                <div className="d-flex justify-content-end align-items-end">
+                  <img src={cardIcon} />
+                </div>
+                <div className="card">
+                  <img src={teamTwo} className="card-img-top" />
+                  <div className="card-body">
+                    <h5 className="card-title text-center">Kenroly Gajon</h5>
+                    <p className="card-text text-center">Food Technician</p>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="d-flex justify-content-center align-items-center my-5">
+              <p style={{ color: "#4D6496" }} className="m-0"><u>Our Valuable Expert Doctors Team</u></p>
+              <Link to="/ourteams" ><button className="btn see-all-doctors mx-3">See all doctors <FaArrowRightLong />
+              </button></Link>
+            </div>
+          </div>
         </div>
       </section>
     </>
