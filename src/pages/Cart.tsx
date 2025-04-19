3e6b9d122eb657e2065d71df402187102172772a
gahramanova <narminhikmat@gmail.com>
2025-04-19 22:57:18 +0200

    Added empty cart image

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -7,10 +7,10 @@ import { FaMinus } from "react-icons/fa";
 import { useCart } from "react-use-cart";
 const Cart = () => {
 
-  const {items, 
-    updateItemQuantity, 
-    cartTotal, 
-    removeItem} = useCart()
+  const { items,
+    updateItemQuantity,
+    cartTotal,
+    removeItem } = useCart()
   return (
     <>
       <section className="section-cart-one">
@@ -30,158 +30,184 @@ const Cart = () => {
           </div>
         </div>
       </section>
-      <section className="section-cart-two my-5">
-        <div className="container">
-          <div className="row g-3">
-            <div className="col-12 col-sm-8 col-md-8 col-lg-8">
-
-              <table className="table table-version-first">
-                <thead>
-                  <tr>
-                    <th scope="col">Product</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Total</th>
-                    <th scope="col"></th>
-                  </tr>
-                </thead>
-                <tbody>
-
-                  {items.map(item =>(
-                    <tr>
-                    <th scope="row"><Link to={"/shop"}><img src={`http://localhost:3025/${item.coverImg?.replace(/\\/g, "/")}`} style={{ width: "250px", height: "200px" }} /></Link></th>
-                    <td>{item.name}</td>
-                    <td>{item.price}$</td>
-                    <td>  <div className="quantity-controls">
-                      <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) - 1)} className="dec mx-2 fw-bold">
-                        <FaMinus />
-                      </button>
-                      <span>{item.quantity}</span>
-                      <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) + 1)} className="inc mx-2 fw-bold">
-                        <FaPlus />
-                      </button>
-                    </div></td>
-                    <td>{(item.price) * (item.quantity??1)}$</td>
-                    <td>
-                      <button onClick={() => removeItem(item.id)} className="btn remove">
-                        <IoIosCloseCircleOutline className="mx-3" style={{ width: "23px", height: "23px", color: "#cccccc" }} />
-                      </button></td>
-                  </tr>
-                  ))}
-
-
-
-
-
-                  {/* <tr>
-                    <th scope="row"><img src={productTwo} style={{ width: "250px", height: "200px" }} /></th>
-                    <td>Arm & Hammer Super Deodori zing Dog Shampoo, Pet Wash</td>
-                    <td>$20.00</td>
-                    <td>  <div className="quantity-controls">
-                      <button className="dec mx-2 fw-bold">
-                        <FaMinus />
-                      </button>
-                      <span>0</span>
-                      <button className="inc mx-2 fw-bold">
-                        <FaPlus />
-                      </button>
-                    </div></td>
-                    <td>$20.00</td>
-                    <td>
-                      <button className="btn remove">
-                        <IoIosCloseCircleOutline className="mx-3" style={{ width: "23px", height: "23px", color: "#cccccc" }} />
-                      </button></td>
-                  </tr>
-                  <tr>
-                    <th scope="row"><img src={productThree} style={{ width: "250px", height: "200px" }} /></th>
-                    <td>Milk-Bone Brushing Chews Daily Dental Dog Treats ...</td>
-                    <td>$36.00</td>
-                    <td>  <div className="quantity-controls">
-                      <button className="dec mx-2 fw-bold">
-                        <FaMinus />
-                      </button>
-                      <span>0</span>
-                      <button className="inc mx-2 fw-bold">
-                        <FaPlus />
-                      </button>
-                    </div></td>
-                    <td>$36.00</td>
-                    <td>
-                      <button className="btn remove">
-                        <IoIosCloseCircleOutline className="mx-3" style={{ width: "23px", height: "23px", color: "#cccccc" }} />
-                      </button></td>
-                  </tr> */}
-                </tbody>
-              </table>
-
-              {/* /////////////////////////////////////////////////////////////////// */}
-
-              <div className="table-version-second">
-                <div className="row g-5">
-                {items.map(item => (
-                    <div className="col-12 col-sm-6 col-md-6 col-lg-6">
-                    <div className="d-flex justify-content-center align-items-center flex-column">
-                      <img src={`http://localhost:3025/${item.coverImg?.replace(/\\/g, "/")}`} style={{ width: "100%", height: "280px" }} alt="..." />
-                    </div>
-                    <h5 className="card-title my-3" style={{ color: "#002169" }}>{item.name}</h5>
-                    <div className="card">
-                      <div className="card-body">
-                        <div className="d-flex justify-content-between border-bottom">
-                          <p style={{ color: "#808080" }} className="card-text">Price</p>
-                          <p style={{ color: "#002169", fontWeight: "500" }} className="card-text">{item.price}$</p>
-                        </div>
-                        <div className="d-flex justify-content-between border-bottom my-3">
-                          <p style={{ color: "#808080" }} className="card-text">Quantity</p>
-                          <p style={{ color: "#002169", fontWeight: "500" }} className="card-text">  <div className="quantity-controls mb-2">
-                          <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) - 1)} className="dec mx-2 fw-bold">
-                        <FaMinus />
-                      </button>
-                      <span>{item.quantity}</span>
-                      <button onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) + 1)} className="inc mx-2 fw-bold">
-                        <FaPlus />
-                      </button>
-                          </div></p>
-                        </div>
-                        <div className="d-flex justify-content-between border-bottom">
-                          <p style={{ color: "#808080" }} className="card-text">Subtotal</p>
-                          <p style={{ color: "#002169", fontWeight: "500" }} className="card-text">{(item.price) * (item.quantity??1)}$</p>
-                        </div>
-
-                        <div className="d-flex justify-content-between align-items-center mt-3">
-                          <Link to="/shop"><button className="btn return-to-shop">Return to shop</button></Link>
-                          <button onClick={() => removeItem(item.id)} className="btn-close"></button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-                </div>
-              </div>
-
-              {/* ///////////////////////////////////////////////////////////////////// */}
+      {items.length === 0 ? (
+        <section className="section-cart-two my-5">
+          <div className="container">
+            <div className="d-flex justify-content-center align-items-center">
+              <img
+                src="https://png.pngtree.com/png-clipart/20250120/original/pngtree-cart-empty-vector-png-image_20275973.png"
+                style={{ width: "350px", height: "350px" }}
+                alt="empty cart"
+              />
             </div>
-            <div className="col-12 col-sm-4 col-md-4 col-lg-4">
-              <div className="content">
-                <h5 className="mb-4">Cart Total</h5>
-                <div className="d-flex justify-content-between border-bottom">
-                  <p style={{ color: "#979797" }}>Subtotal:</p>
-                  <p style={{ color: "#002169", fontWeight: "500" }}>{cartTotal} $</p>
-                </div>
-                <div className="d-flex justify-content-between border-bottom mt-3">
-                  <p style={{ color: "#979797" }}>Shipping:</p>
-                  <p style={{ color: "#002169", fontWeight: "500" }}>Free</p>
-                </div>
-                <div className="d-flex justify-content-between mt-3">
-                  <p style={{ color: "#979797" }}>Total:</p>
-                  <p style={{ color: "#002169", fontWeight: "500" }}>{cartTotal} $</p>
-                </div>
-                <button className="btn button-checkout d-flex align-items justify-content-center mt-3">Proceed to checkout</button>
+            <div className="row g-3">
+              <div className="col-12 col-sm-8 col-md-8 col-lg-8">
+
               </div>
             </div>
           </div>
+        </section>
+      ) : (
+        <>
+          <table className="table table-version-first">
+            <thead>
+              <tr>
+                <th scope="col">Product</th>
+                <th scope="col">Name</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Total</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item.id}>
+                  <th scope="row">
+                    <Link to={"/shop"}>
+                      <img
+                        src={`http://localhost:3025/${item.coverImg?.replace(
+                          /\\/g,
+                          "/"
+                        )}`}
+                        style={{ width: "270px", height: "200px" }}
+                        alt={item.name}
+                      />
+                    </Link>
+                  </th>
+                  <td>{item.name}</td>
+                  <td>{item.price}$</td>
+                  <td>
+                    <div className="quantity-controls">
+                      <button
+                        onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) - 1)}
+                        className="dec mx-2 fw-bold"
+                      >
+                        <FaMinus />
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button
+                        onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) + 1)}
+                        className="inc mx-2 fw-bold"
+                      >
+                        <FaPlus />
+                      </button>
+                    </div>
+                  </td>
+                  <td>{item.price * (item.quantity ?? 1)}$</td>
+                  <td>
+                    <button onClick={() => removeItem(item.id)} className="btn remove">
+                      <IoIosCloseCircleOutline
+                        className="mx-3"
+                        style={{ width: "23px", height: "23px", color: "#cccccc" }}
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          {/* Mobile version of the table */}
+          <div className="table-version-second">
+            <div className="row g-5">
+              {items.map((item) => (
+                <div key={item.id} className="col-12 col-sm-6 col-md-6 col-lg-6">
+                  <div className="d-flex justify-content-center align-items-center flex-column">
+                    <img
+                      src={`http://localhost:3025/${item.coverImg?.replace(
+                        /\\/g,
+                        "/"
+                      )}`}
+                      style={{ width: "100%", height: "280px" }}
+                      alt={item.name}
+                    />
+                  </div>
+                  <h5 className="card-title my-3" style={{ color: "#002169" }}>
+                    {item.name}
+                  </h5>
+                  <div className="card">
+                    <div className="card-body">
+                      <div className="d-flex justify-content-between border-bottom">
+                        <p style={{ color: "#808080" }} className="card-text">
+                          Price
+                        </p>
+                        <p style={{ color: "#002169", fontWeight: "500" }} className="card-text">
+                          {item.price}$
+                        </p>
+                      </div>
+                      <div className="d-flex justify-content-between border-bottom my-3">
+                        <p style={{ color: "#808080" }} className="card-text">
+                          Quantity
+                        </p>
+                        <p style={{ color: "#002169", fontWeight: "500" }} className="card-text">
+                          <div className="quantity-controls mb-2">
+                            <button
+                              onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) - 1)}
+                              className="dec mx-2 fw-bold"
+                            >
+                              <FaMinus />
+                            </button>
+                            <span>{item.quantity}</span>
+                            <button
+                              onClick={() => updateItemQuantity(item.id, (item.quantity ?? 0) + 1)}
+                              className="inc mx-2 fw-bold"
+                            >
+                              <FaPlus />
+                            </button>
+                          </div>
+                        </p>
+                      </div>
+                      <div className="d-flex justify-content-between border-bottom">
+                        <p style={{ color: "#808080" }} className="card-text">
+                          Subtotal
+                        </p>
+                        <p style={{ color: "#002169", fontWeight: "500" }} className="card-text">
+                          {item.price * (item.quantity ?? 1)}$
+                        </p>
+                      </div>
+
+                      <div className="d-flex justify-content-between align-items-center mt-3">
+                        <Link to="/shop">
+                          <button className="btn return-to-shop">Return to shop</button>
+                        </Link>
+                        <button onClick={() => removeItem(item.id)} className="btn-close"></button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+
+      {/* Cart Summary (Only visible if items are present) */}
+      {items.length > 0 && (
+        <div className="col-12 col-sm-4 col-md-4 col-lg-4">
+          <div className="content">
+            <h5 className="mb-4">Cart Total</h5>
+            <div className="d-flex justify-content-between border-bottom">
+              <p style={{ color: "#979797" }}>Subtotal:</p>
+              <p style={{ color: "#002169", fontWeight: "500" }}>{cartTotal} $</p>
+            </div>
+            <div className="d-flex justify-content-between border-bottom mt-3">
+              <p style={{ color: "#979797" }}>Shipping:</p>
+              <p style={{ color: "#002169", fontWeight: "500" }}>Free</p>
+            </div>
+            <div className="d-flex justify-content-between mt-3">
+              <p style={{ color: "#979797" }}>Total:</p>
+              <p style={{ color: "#002169", fontWeight: "500" }}>{cartTotal} $</p>
+            </div>
+            <button className="btn button-checkout d-flex align-items justify-content-center mt-3">
+              Proceed to checkout
+            </button>
+          </div>
         </div>
-      </section>
+      )}
+
+
+
     </>
   )
 }
